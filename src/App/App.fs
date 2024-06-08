d972126441354c199ce992905ef582b39a1ac543
Goswin Rothenthal <goswin@rothenthal.com>
2024-06-08 15:52:45 +0200

    fixes for  updated solidjs/router 0.13.5

--- src/App/App.fs
+++ src/App/App.fs
@@ -1,68 +1,8 @@
 module App
 
 open Browser
-open Feliz.JSX.Solid
-open Fable.Core
-open Solid
-open type Components
 
-type Lazy =
-    static member Counter() = Solid.lazyImport Counter
-    static member Resource() = Solid.lazyImport Resource
-    static member Svg() = Solid.lazyImport Svg
-    static member Sketch() = Solid.lazyImport Sketch
-    static member TodoElmish() = Solid.lazyImport TodoElmish
-    static member Shoelace() = Solid.lazyImport Shoelace
+Solid.render (Routing.Routing, document.getElementById ("app-container"))
 
-[<JSX.Component>]
-let Tab(href: string, txt: string) =
-    Html.li (Router.Link(href, Html.text txt))
-
-[<JSX.Component>]
-let Tabs() =
-    Html.fragment [
-        Html.div [
-            Attr.className "tabs"
-            Attr.style [
-                Css.marginBottom (length.rem 0.5)
-            ]
-            Html.children [
-                Html.ul [
-                    Html.children [
-                        Tab("/counter", "Counter")
-                        Tab("/resource", "Resource")
-                        Tab("/svg", "Svg")
-                        Tab("/sketch", "Sketch")
-                        Tab("/elmish", "Todo Elmish")
-                        Tab("/shoelace", "Shoelace Web Components")
-                    ]
-                ]
-            ]
-        ]
-        Html.div [
-            Attr.style [
-                Css.margin (length.zero, length.auto)
-                Css.maxWidth 800
-                Css.padding 20
-            ]
-            Html.children [
-                Router.Routes [
-                    Router.Route("/", ``component`` = Lazy.Counter)
-                    Router.Route("/counter", ``component`` = Lazy.Counter)
-                    Router.Route("/resource", ``component`` = Lazy.Resource)
-                    Router.Route("/svg", ``component`` = Lazy.Svg)
-                    Router.Route("/sketch", ``component`` = Lazy.Sketch)
-                    Router.Route("/elmish", ``component`` = Lazy.TodoElmish)
-                    Router.Route("/shoelace", ``component`` = Lazy.Shoelace)
-                ]
-            ]
-        ]
-    ]
-
-[<JSX.Component>]
-let App() =
-    Router.Router(
-        ColorProvider("green", Tabs())
-    )
-
-Solid.render((fun () -> Router.Router(App())), document.getElementById("app-container"))
+// Keeping this entry file simple so that it does not get changed .
+// This helps to avoid an issue where components are rendered as duplicates while watching and reloading with Vite
